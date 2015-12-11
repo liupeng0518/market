6884b464c8496f5ab8cd4b1a2a1d20173866a6ca
刘鹏 <liupeng0518@gmail.com>
2015-12-11 15:54:42 +0800

    urls change

--- src/products/urls.py
+++ src/products/urls.py
@@ -3,13 +3,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
-=======
->>>>>>> parent of 8fb481b... merge the first
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
